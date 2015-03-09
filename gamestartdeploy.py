885413dd8e6978bf68f6e087a03bf7f74f36739d
heyandy889 <heyandy889@users.noreply.github.com>
2015-03-09 16:55:18 -0400

    adding web client and its deployment

--- gamestartdeploy.py
+++ gamestartdeploy.py
@@ -1,4 +1,3 @@
-
 import os
 import shutil
 import stat
@@ -6,24 +5,26 @@ import subprocess
 import sys
 import time
 
-print('Caution, replacing plug-in:')
+print('Caution, replacing plug-in and editor:')
 for i in range(9,0,-1):
 	print(i)
 	time.sleep(1)
 
-pathToMacuyikoProject = '../MinecraftPythonConsole-fork'
-pathToServerPlugin = os.path.join(pathToMacuyikoProject,'ServerPythonInterpreterPlugin')
-
+# = = =
+pathToMacuyikoProject 	= '../MinecraftPythonConsole-fork'
+pathToServerPlugin 		= os.path.join(pathToMacuyikoProject,'ServerPythonInterpreterPlugin')
+pathToWebEditor 		= os.path.join(pathToMacuyikoProject,'ServerEditorWeb')
 if not os.path.exists(pathToServerPlugin):
 	print('Oops! Missing Macuyiko\'s code. Clone from gamestartdev on github for a stable version.')
 	sys.exit(1)
-	
+
+SERVER_PATH = os.path.join('.','server')
+stuffToCopy = ['lib-common','lib-canary','python']
+
 def exterminate(function, path):
 	os.chmod(path, stat.S_IWRITE)
 	os.remove(path)
-	
-SERVER_PATH = os.path.join('.','server')	
-stuffToCopy = ['lib-common','lib-canary','python']
+
 for lib in stuffToCopy:
 	source = os.path.join(pathToServerPlugin, lib)
 	destination = os.path.join(SERVER_PATH, lib)
@@ -31,13 +32,11 @@ for lib in stuffToCopy:
 		shutil.rmtree(destination,onerror=exterminate)
 	shutil.copytree( source, destination )
 
-
 #folder structure is silly. we need the server jar at the root.
 jarname 	= 'CanaryMod-1.2.0.jar'
 jarsource 	= os.path.join(SERVER_PATH ,'lib-canary',jarname)
 jardest 	= os.path.join(SERVER_PATH, jarname)
 shutil.move(jarsource, jardest)
-
 
 pluginsource	= os.path.join(pathToServerPlugin, 'dist', 'console.jar')
 pluginsfolder	= os.path.join(SERVER_PATH, 'plugins')
@@ -45,5 +44,12 @@ if not os.path.exists(pluginsfolder):
 	os.mkdir(pluginsfolder)
 shutil.copyfile(pluginsource, os.path.join(pluginsfolder,'console.jar'))
 
+# = = =
+CLIENT_PATH = os.path.join('.','client')
+if os.path.exists(CLIENT_PATH):
+	shutil.rmtree(CLIENT_PATH,onerror=exterminate)
+shutil.copytree(pathToWebEditor, CLIENT_PATH)
 
-print('Done! Ready to launch server from directory: ' + SERVER_PATH)
+print('Ready to launch server from directory: ' + SERVER_PATH)
+print('Ready to launch client from directory: ' + CLIENT_PATH)
+print('Done!')
