b7f98627d2799e60712eb0e084b82313e024f09f
heyandy889 <you@example.com>
2015-03-09 00:49:10 -0400

    a little more generous on the countdown

--- gamestartdeploy.py
+++ gamestartdeploy.py
@@ -1,6 +1,14 @@
 
 import os
 import shutil
+import time
+
+
+countdown = 10
+print('WARNING! This is about to reset the server! Halt in the next ' + str(countdown) + ' seconds to stop!')
+for i in range(countdown,-1,-1):
+	print(i)
+	time.sleep(1)
 
 pathToMacuyikoProject = '../MinecraftPythonConsole-fork'
 pathToServerPlugin = os.path.join(pathToMacuyikoProject,'ServerPythonInterpreterPlugin')
@@ -8,10 +16,11 @@ pathToServerPlugin = os.path.join(pathToMacuyikoProject,'ServerPythonInterpreter
 stuffToCopy = ['lib-common','lib-canary','python']
 
 
-SERVER_PATH = 'server'
+SERVER_PATH = os.path.join('.','server')
 if os.path.exists(SERVER_PATH):
 	shutil.rmtree(SERVER_PATH)	#for some reason shutil.copytree freaks out if destination already exists. for safety I guess.
-
+os.mkdir(SERVER_PATH)
+	
 for lib in stuffToCopy:
 	source = os.path.join(pathToServerPlugin, lib)
 	destination = os.path.join(SERVER_PATH, lib)
@@ -22,5 +31,12 @@ jarname 	= 'CanaryMod-1.2.0.jar'
 jarsource 	= os.path.join(SERVER_PATH ,'lib-canary',jarname)
 jardest 	= os.path.join(SERVER_PATH, jarname)
 shutil.move(jarsource, jardest)
+
+
+pluginsource	= os.path.join(pathToServerPlugin, 'dist', 'console.jar')
+pluginsfolder	= os.path.join(SERVER_PATH, 'plugins')
+if not os.path.exists(pluginsfolder):
+	os.mkdir(pluginsfolder)
+shutil.copyfile(pluginsource, os.path.join(pluginsfolder,'console.jar'))
 
 print('Done! Ready to launch server from directory: ' + SERVER_PATH)
