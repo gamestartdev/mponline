d73bec2aa539c1a38dabe573b2a35e843d0478d2
heyandy889 <heyandy889@users.noreply.github.com>
2015-05-01 01:07:23 -0400

    moving actual server launch into crontab script, instead of separate python script

--- server/start-server.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.call(['java', '-jar', 'CanaryMod-1.2.0.jar'])
--- server/start-server.sh
+++ server/start-server.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#install with crontab -e
-#*/1 * * * * /home/ubuntu/mponline/server/cron-watchdog.sh
+#install with "crontab crontab-entry.txt"
 
 ps auxw | grep java | grep -v grep > /dev/null
 
@@ -8,5 +7,6 @@ if [ $? != 0 ]
 then
 	cd ~/mponline/server
 	touch cron-was-here.txt
-	python start-server.py
+	java -jar CanaryMod-1.2.0.jar
 fi
+
